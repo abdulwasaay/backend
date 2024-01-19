38e3bc85dd0d6f072af5a15540d8c834cba745c4
Abdul Wasay <wasaysaleem071@gmail.com>
2024-01-19 07:14:29 -0800

    a

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,11 +1,5 @@
-import { addData } from "@/methods/formMethods";
 
-export async function POST(request){
-    const  {email,password,check} = await request.json();
-    try{
-        await addData(email,password,check)
-        return Response.json({mess:"api"})
-    }catch(err){
-        return Response.json({mess:err})
-    }
+
+export async function GET(request){
+     return Response.json({mess:"api"})
 }
